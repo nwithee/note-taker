c797ace5df48249f1878ef4ad10f5a26968e9b0d
nzwithee@gmail.com <Cartoon#01>
2022-04-20 16:28:38 -0500

    apiroutes basic setup completed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const htmlRoutes = require('./routes/htmlRoutes');
-//const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(express.urlencoded( { extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-//app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 //Code for listener
