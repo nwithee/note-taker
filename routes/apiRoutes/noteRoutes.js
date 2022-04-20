583c446a052acb568b1e011737c8c69435b0caef
nzwithee@gmail.com <Cartoon#01>
2022-04-20 17:42:50 -0500

    displaying notes finally

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,4 +1,16 @@
 const router = require('express').Router();
-//const db = require('../db/db');
+const path = require ('path');
+const fs = require ('fs');
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../db/db.json'));
+});
+
+//router.post('/notes')
+
+
+
+
+
 
 module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -9,4 +9,8 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
 module.exports = router;
