9e135425d0c1ff04c7891d72989faff039a2508a
nzwithee@gmail.com <Cartoon#01>
2022-04-20 16:11:47 -0500

    implemented routing for HTML

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+
+const htmlRoutes = require('./routes/htmlRoutes');
+//const apiRoutes = require('./routes/apiRoutes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //parse incoming data
 app.use(express.urlencoded( { extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
+//app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 //Code for listener
 app.listen(PORT, () => {
-    console.log(`--You server is ready on port ${PORT}--`);
+    console.log(`--Your server is ready on port ${PORT}--`);
 });
